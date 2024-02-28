7102d76f3520e1285b7c0132480b349cb979526c
Sammy-Jo Wymer <sammy-jo.wymer@diconium.com>
2024-02-28 09:07:50 +0100

    refactor(app): improve layout and functionality of frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const AppBackend = () => {
+const App = () => {
   const [response, setResponse] = useState<string>(
     "Hi there! How can I assist you?"
   );
   const [value, setValue] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
 
   const handleSubmit = async () => {
     const response = await axios.post("http://localhost:3005/chatbot", {
@@ -16,22 +19,20 @@ const AppBackend = () => {
   };
 
   return (
-    <div className="container">
-      <div>
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        ></input>
+    <>
+      <div className="container">
+        <div>
+          <input type="text" value={value} onChange={onChange}></input>
+        </div>
+        <div>
+          <button onClick={handleSubmit}>Click me for answers!</button>
+        </div>
+        <div>
+          <p>Chatbot: {response}</p>
+        </div>
       </div>
-      <div>
-        <button onClick={handleSubmit}>Click me for answers!</button>
-      </div>
-      <div>
-        <p>Chatbot: {response}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
-export default AppBackend;
+export default App;
