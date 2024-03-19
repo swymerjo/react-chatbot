812203b298f24cd31713b50c964b669569b9805e
Sammy-Jo Wymer <sammy-jo.wymer@diconium.com>
2024-03-19 13:59:30 +0100

    refactor(app): isolate chatbot component

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import "../App.css";
+import React from "react";
+
+const Chatbot = () => {
+  const [response, setResponse] = useState<string>(
+    "Hi there! How can I assist you?"
+  );
+  const [value, setValue] = useState<string>("");
+  const [question, setQuestion] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const response = (await axios.post("/chat", { question: value })).data
+        .choices[0].message.content;
+      setResponse(response);
+      setLoading(false);
+      setQuestion(value);
+    } catch (error) {
+      console.log("Error details:", error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          onFocus={() => setValue("")}
+        ></input>
+      </div>
+      <div>
+        <button onClick={handleSubmit} disabled={!value.trim()}>
+          {loading ? "Just one sec, lad..." : "Click me for answers!"}
+        </button>
+      </div>
+      <div>
+        <p className="box">Chatbot: {response}</p>
+      </div>
+    </>
+  );
+};
+
+export default Chatbot;
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,13 @@
-"use client";
-
-import { useState } from "react";
-import axios from "axios";
 import "./App.css";
 import React from "react";
+import Chatbot from "./chatbot/page";
 
 const App = () => {
-  const [response, setResponse] = useState<string>(
-    "Hi there! How can I assist you?"
-  );
-  const [value, setValue] = useState<string>("");
-  const [question, setQuestion] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setValue(e.target.value);
-
-  const handleSubmit = async () => {
-    setLoading(true);
-    try {
-      const response = (await axios.post("/chat", { question: value })).data
-        .choices[0].message.content;
-      setResponse(response);
-      setLoading(false);
-      setQuestion(value);
-    } catch (error) {
-      console.log("Error details:", error);
-    }
-  };
-
   return (
     <>
       <h1 className="header">🔴 ChatLFC ⚽️</h1>
       <div className="container">
-        <div>
-          <input
-            type="text"
-            value={value}
-            onChange={onChange}
-            onFocus={() => setValue("")}
-          ></input>
-        </div>
-        <div>
-          <button onClick={handleSubmit} disabled={!value.trim()}>
-            {loading ? "Just one sec, lad..." : "Click me for answers!"}
-          </button>
-        </div>
-        <div>
-          <p className="box">Chatbot: {response}</p>
-        </div>
+        <Chatbot />
       </div>
       <p className="box disclaimer">
         This chatbot provides information about results and goal contributions
