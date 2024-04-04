a565501bbc4041f4bbad7ea967018bac661dab8a
Sammy-Jo Wymer <sammy-jo.wymer@diconium.com>
2024-04-04 13:50:06 +0200

    feat(app): store and show all messages on page

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -6,12 +6,15 @@ import "../App.css";
 import React from "react";
 
 function Chatbot() {
-  const [response, setResponse] = useState<string>(
-    "Hi there! How can I assist you?"
-  );
+  const [response, setResponse] = useState<string>();
   const [value, setValue] = useState<string>("");
   const [question, setQuestion] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
+  const [messages, setMessages] = useState<any>([
+    {
+      chatbot: "Hi there how can I assist you?",
+    },
+  ]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value);
@@ -21,13 +24,21 @@ function Chatbot() {
     if (!value.trim()) return;
     setLoading(true);
     try {
-      const response = (await axios.post("/api/openai", { question: value }))
-        .data.choices[0].message.content;
+      const response = (
+        await axios.post("/api/openai", {
+          question: value,
+        })
+      ).data.choices[0].message.content;
       setResponse(response);
-      setLoading(false);
-      setQuestion(value);
+      setMessages((prevMessages: any) => [
+        ...prevMessages,
+        { chatbot: response, user: value },
+      ]);
     } catch (error) {
       console.log("Error details:", error);
+    } finally {
+      setLoading(false);
+      setQuestion(value);
     }
   };
 
@@ -42,14 +53,32 @@ function Chatbot() {
           onChange={onChange}
           onFocus={() => setValue("")}
           tabIndex={0}
+          placeholder="What would you like to ask?"
         ></input>
         <button type="submit" tabIndex={0}>
           {loading ? "Just one sec, lad..." : "Click me for answers!"}
         </button>
       </form>
-      <p className="box" tabIndex={0}>
-        Chatbot: {response}
-      </p>
+      {messages.map((message: any, index: number) => {
+        return (
+          <React.Fragment key={index}>
+            {message.user ? (
+              <>
+                <p className="box user" tabIndex={0}>
+                  You: {message.user}
+                </p>
+                <p className="box" tabIndex={0}>
+                  Darwizzy: {message.chatbot}
+                </p>
+              </>
+            ) : (
+              <p className="box" tabIndex={0}>
+                Darwizzy: {message.chatbot}
+              </p>
+            )}
+          </React.Fragment>
+        );
+      })}
     </main>
   );
 }
